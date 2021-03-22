ac2d026ef0dee23ce1e8a9e701d8579a085bd2f8
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-22 11:55:43 +0300

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+##Domain Interact Menu Script
+timestamp=$(date +%Y-%m-%d:%H:%M)
+domain="$1"
+	mkdir /root/Cases/$domain
+		amass intel -whois -ip -src -d $domain  -o /root/Cases/$domain/$timestamp-$domain.1.txt 
+		amass enum -src -brute -d $domain -o /root/Cases/$domain/$timestamp-$domain.2.txt -d $domain
+			sleep 3
+			sublist3r -d $domain -o /root/Cases/$domain/sublist3r_$domain.txt
+				sleep 3
+				photon -u $domain -l 3 -t 100 -o /root/Cases/$domain/$timestamp-$domain
+					sleep 3
+					theHarvester -d $domain -b baidu,bing,google,yahoo,virustotal,certspotter,crtsh,dnsdumpster,duckduckgo,exalead,hackertarget,intelx,hunter,netcraft,otx,pentesttools,projectdiscovery,qwant,rapiddns,securityTrails,sublist3r,threatcrowd,threatminer,trello,twitter,urlscan -f /root/Cases/$domain/$timestamp-$domain.html
+						firefox /root/Cases/$domain/$timestamp-$domain.html
+							thunar "/root/Cases/$domain" >/dev/null 2>&1
+		
