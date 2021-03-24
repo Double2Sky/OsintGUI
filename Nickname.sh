51d2760c476d622d644f0c438b202077e473dad2
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-24 23:14:18 +0300

    Create Nickname.sh

---
+++ Nickname.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+NICKNAME=$(zenity --entry --text="Enter NickName")
+mkdir /root/Cases/Nickname/$NICKNAME
+cd /root/Cases/Nickname/$NICKNAME
+	maigret $NICKNAME -a -T
+		sleep 5
+		Investigo $NICKNAME -s -d
+			sleep 5
+			search4 -u $NICKNAME
+				thunar '/root/Cases/Nickname/$NICKNAME'
