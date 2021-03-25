49d2c16e050b308c5c7ca02aacd6e5297b79646b
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-25 22:41:22 +0300

    Update Nickname.sh

--- Nickname.sh
+++ Nickname.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 NICKNAME=$(zenity --entry --text="Enter NickName")
+mkdir /root/Cases/Nickname
 mkdir /root/Cases/Nickname/$NICKNAME
 cd /root/Cases/Nickname/$NICKNAME
 	maigret $NICKNAME -a -T
 		sleep 5
 		Investigo $NICKNAME -s -d
 			sleep 5
-			search4 -u $NICKNAME | >> output.txt
+			search4 -u $NICKNAME | tee -a output.txt
 				thunar /root/Cases/Nickname/$NICKNAME
