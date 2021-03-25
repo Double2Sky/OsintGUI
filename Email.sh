fefd43d28a47a4dc4435885eafbe9c1f47fcff8b
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-25 23:09:26 +0300

    Create Email.sh

---
+++ Email.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+EMAIL=$(zenity --entry --text="Enter E-mail")
+mkdir /root/Cases/Email
+mkdir /root/Cases/Email/$EMAIL
+cd /root/Cases/Nickname/$EMAIL
+	holehe $EMAIL --only-used -C
+		sleep 3
+		h8mail -t $EMAIL -o output
