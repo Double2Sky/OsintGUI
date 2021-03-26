e32789e34db4630a9cf5c4bf2afd2a1e19d2e54a
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-26 13:35:12 +0300

    Update Email.sh

--- Email.sh
+++ Email.sh
@@ -2,7 +2,7 @@
 
 EMAIL=$(zenity --entry --text="Enter E-mail")
 mkdir /root/Cases/Email
-mkdir /root/Cases/Email/$EMAILhttps://github.com/Double2Sky/OsintGUI/blob/main/Email.sh
+mkdir /root/Cases/Email/$EMAIL
 cd /root/Cases/Email/$EMAIL
 	holehe $EMAIL --only-used -C
 		sleep 3
