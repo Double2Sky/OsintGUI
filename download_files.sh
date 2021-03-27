8ca607b58a3f404febef7a1cc4dadbb96a847193
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-27 17:55:35 +0300

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -5,4 +5,8 @@ mkdir /root/Cases/Download_Files/$DOMAIN
 cd /root/Cases/Download_Files/$DOMAIN
 	metagoofil -d $DOMAIN -w -t pdf,doc,xls,ppt,docx,xlsx,pptx -o /root/Cases/Download_Files/$DOMAIN
 		sleep 5
-		python3 /opt/deluxe/deluxe.py search -d $DOMAIN -n 200 -o /root/Cases/Download_Files/$DOMAIN
+		exiftool /root/Cases/Download_Files/$DOMAIN/* | tee -a metadata1.txt
+			sleep 5
+			python3 /opt/deluxe/deluxe.py search -d $DOMAIN -n 200 -o /root/Cases/Download_Files/$DOMAIN
+				sleep 5
+				python3 /opt/deluxe/deluxe.py extract -m /root/Cases/Download_Files/$DOMAIN/manifest.json -o /root/Cases/Download_Files/$DOMAIN/metadata2.txt  
