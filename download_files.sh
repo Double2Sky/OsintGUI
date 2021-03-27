e8a906e9f89870ee5a7bab15649523e68eb8d922
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-27 23:10:34 +0300

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-DOMAIN=$(zenity --entry --text=Enter Domain)
-mkdir /root/Cases/Download_Files/
-mkdir /root/Cases/Download_Files/$DOMAIN
-cd /root/Cases/Download_Files/$DOMAIN
-	metagoofil -d $DOMAIN -w -t pdf,doc,xls,ppt,docx,xlsx,pptx -o /root/Cases/Download_Files/$DOMAIN
+
+DOMAIN=$(zenity --entry --title "Metagoofil" --text="Enter Domain" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
+
+	mkdir /root/Cases/Download_Files/
+	mkdir /root/Cases/Download_Files/$DOMAIN
+	cd /root/Cases/Download_Files/$DOMAIN
+	
+	mkdir metagoofil
+		metagoofil -d $DOMAIN -w -e 60 -i 30 -t pdf,doc,xls,ppt,docx,xlsx,pptx -o /root/Cases/Download_Files/$DOMAIN/metagoofil/
 		sleep 5
-		exiftool /root/Cases/Download_Files/$DOMAIN/* | tee -a metadata1.txt
-			sleep 5
-			python3 /opt/deluxe/deluxe.py search -d $DOMAIN -n 200 -o /root/Cases/Download_Files/$DOMAIN
-				sleep 5
-				python3 /opt/deluxe/deluxe.py extract -m /root/Cases/Download_Files/$DOMAIN/manifest.json -o /root/Cases/Download_Files/$DOMAIN/metadata2.txt  
+		exiftool /root/Cases/Download_Files/$DOMAIN/metagoofil/* | tee -a metadata.txt
+		sleep 30
+	mkdir deluxe				
+		python3 /opt/deluxe/deluxe.py search -d $DOMAIN -n 200 -o /root/Cases/Download_Files/$DOMAIN/deluxe/
+		sleep 5
+		python3 /opt/deluxe/deluxe.py extract -m /root/Cases/Download_Files/$DOMAIN/deluxe/manifest.json -o /root/Cases/Download_Files/$DOMAIN/deluxe/metadata.txt  
+		sleep 30		
+	mkdir pymeta
+		pymeta -d $DOMAIN -s all -m 50 -j 5 -o /root/Cases/Download_Files/$DOMAIN/pymeta/
+		
+thunar "/root/Cases/Download_Files/$DOMAIN" >/dev/null 2>&1 
