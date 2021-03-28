0d3982bbdafea2f017782e03b82f573904556d78
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-28 20:24:20 +0300

    Update DownloadSite.sh

--- DownloadSite.sh
+++ DownloadSite.sh
@@ -3,5 +3,5 @@ SITE=$(zenity --entry --text="Enter URL (withot http://)")
 mkdir /root/Cases/DownloadSite
 mkdir /root/Cases/DownloadSite/$SITE
 cd /root/Cases/DownloadSite/$SITE
-	httrack http://$SITE O root/Cases/DownloadSite/$SITE
+	httrack http://$SITE O
 	thunar /root/Cases/DownloadSite/$SITE
