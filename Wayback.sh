b1d9bac1c9bdea2fc16445cb4528c0547dd05ce9
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-28 19:59:48 +0300

    Update Wayback.sh

--- Wayback.sh
+++ Wayback.sh
@@ -2,10 +2,11 @@
 DOMAIN=$(zenity --entry --text="Enter DOMAIN")
 mkdir /root/Cases/Wayback
 mkdir /root/Cases/Wayback/$DOMAIN
+mkdir /root/Cases/Wayback/$DOMAIN/DataTime 
 cd /root/Cases/Wayback/$DOMAIN
 	/root/go/bin/waybackurls $DOMAIN | tee -a output.txt
 		sleep 5
-		waybackpack $DOMAIN -d /root/Cases/Wayback/$DOMAIN --from-date 1990 --follow-redirects
+		waybackpack $DOMAIN -d /root/Cases/Wayback/$DOMAIN/DataTime --from-date 1990 --follow-redirects
 			sleep 5
 			wayback_machine_downloader $DOMAIN
 			thunar /root/Cases/Wayback/$DOMAIN
