395f10907aac5e93b015575556e669fc32f3a3db
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-28 19:26:46 +0300

    Update Wayback.sh

--- Wayback.sh
+++ Wayback.sh
@@ -5,4 +5,7 @@ mkdir /root/Cases/Wayback/$DOMAIN
 cd /root/Cases/Wayback/$DOMAIN
 	/root/go/bin/waybackurls $DOMAIN | tee -a output.txt
 		sleep 5
-		wayback_machine_downloader $DOMAIN
+		waybackpack $DOMAIN -d /root/Cases/Wayback/$DOMAIN --from-date 1990 --follow-redirects
+			sleep 5
+			wayback_machine_downloader $DOMAIN
+			thunar /root/Cases/Wayback/$DOMAIN
