60b4339e926e55345cc8c7c7577c6a91137a41fe
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-03-28 20:06:11 +0300

    Create DownloadSite.sh

---
+++ DownloadSite.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+SITE=$(zenity --entry --text="Enter URL (with http://)")
+mkdir /root/Cases/DownloadSite
+mkdir /root/Cases/DownloadSite/$SITE
+cd /root/Cases/DownloadSite/$SITE
+	httrack $SITE
+	thunar /root/Cases/DownloadSite/$SITE
