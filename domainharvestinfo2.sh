d2fb15183aca952d1ce4679ced63b8c8e0c41b59
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-04-03 16:49:56 +0300

    Update domainharvestinfo2.sh

--- domainharvestinfo2.sh
+++ domainharvestinfo2.sh
@@ -2,14 +2,14 @@
 domain=$(zenity --entry --text="Enter Domain")
 mkdir /root/Cases/$domain
 cd /opt/gasmask/
-	python3 gasmask.py -d $domain -i vhosts,google,bing,yahoo,ask,dogpile,twitter,youtube,reddit,github,instagram,crt,pgp,netcraft,virustotal,dnsdump,spyse | tee -a /root/Cases/$domain/output.txt
+	python3 gasmask.py -d $domain -i vhosts,google,bing,yahoo,ask,dogpile,twitter,youtube,reddit,github,instagram,crt,pgp,netcraft,virustotal,dnsdump,spyse | tee -a /root/Cases/$domain/output.$domain.txt
 	cd /root/Cases/$domain
-	gedit output.txt
+	gedit output.$domain.txt
 		sleep 3
-		python3 /opt/OSINT-SPY/osint-spy.py --domain $domain | tee -a output2.txt
-		gedit output2.txt
+		python3 /opt/OSINT-SPY/osint-spy.py --domain $domain | tee -a output2.$domain.txt
+		gedit output2.$domain.txt
 			sleep 3
-			/root/go/bin/gospider -s http://$domain --sitemap -a -w -r -v | tee -a output3.txt
-			gedit output3.txt
+			/root/go/bin/gospider -s http://$domain --sitemap -a -w -r -v | tee -a output3.$domain.txt
+			gedit output3.$domain.txt
 sleep 3
 thunar /root/Cases/$domain
