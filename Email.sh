b744b692ebf2d8b9437fb1a827027c003d3d2c65
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-04-04 13:27:05 +0300

    Update Email.sh

--- Email.sh
+++ Email.sh
@@ -4,14 +4,16 @@ EMAIL=$(zenity --entry --text="Enter E-mail")
 mkdir /root/Cases/Email
 mkdir /root/Cases/Email/$EMAIL
 cd /root/Cases/Email/$EMAIL
-	holehe $EMAIL --only-used -C
-		sleep 3
-		h8mail -t $EMAIL -o output
+	curl emailrep.io/$EMAIL | tee -a output.txt
+		holehe $EMAIL --only-used -C
 			sleep 3
-			socialscan $EMAIL | tee -a output.txt
+			h8mail -t $EMAIL -o output.txt
 				sleep 3
-				python3 /opt/OSINT-SPY/osint-spy.py --email $EMAIL | tee -a output2.txt
+				socialscan $EMAIL | tee -a output.txt
 					sleep 3
-					thunar "/root/Cases/Email/$EMAIL"
+					python3 /opt/OSINT-SPY/osint-spy.py --email $EMAIL | tee -a output.txt
 						sleep 3
-						/opt/scriptnOsint/osint -e $EMAIL
+						thunar "/root/Cases/Email/$EMAIL"
+							sleep 3
+							/opt/scriptnOsint/osint -e $EMAIL
+						
