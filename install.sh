978d7657e70bd0d0851eafd47be84ab2026fa562
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-04-25 21:08:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,20 @@ sudo apt install -y golang
 sudo apt install -y net-tools
 sudo apt install -y ruby
 sudo apt install -y php
-sudo apt install -y 
+sudo apt install -y torbrowser-launcher
 sudo apt install -y 
 sudo apt install -y 
 sudo apt install -y 
 sudo apt install -y 
 sudo apt install -y 
 
+python3 -m pip install osrframework --upgrade
+python3 -m pip install  h8mail --upgrade
 python3 -m pip install arachnid-spider==0.9.4
+
+export GO111MODULE=on
+
+
 
 git clone https://github.com/leohab75/anonsurf_GUI.git ~/Programs 
 sudo chmod +x ~/Programs/anonsurf_GUI/install.sh
@@ -36,7 +42,6 @@ sudo -H pip3 install -r ~/Programs/Photon/requirements.txt
 git clone https://github.com/opsdisk/metagoofil.git ~/Programs/metagoofil
 sudo -H pip3 install -r ~/Programs/metagoofil/requirements.txt
 
-git clone https://github.com/six2dez/reconftw ~/Programs/reconftw
 
 git clone https://github.com/sherlock-project/sherlock.git ~/Programs/sherlock
 python3 -m pip install -r ~/Programs/sherlock/requirements.txt
@@ -45,14 +50,95 @@ git clone https://github.com/smicallef/spiderfoot.git ~/Downloads/Programs/spide
 sudo -H pip3 install -r ~/Programs/spiderfoot/requirements.txt
 
 git clone https://github.com/soxoj/maigret ~/Programs/maigret
-pip3 install ~/Programs/maigret/.
+sudo pip3 install ~/Programs/maigret/.
 
 git clone https://github.com/megadose/holehe.git ~/Programs/holehe
 cd ~/Programs/holehe && sudo python3 setup.py install
 cd
 
 
+git clone https://github.com/thewhiteh4t/FinalRecon ~/Programs/FinalRecon
+sudo -H pip3 install -r  ~/Programs/FinalRecon/requirements.txt
 
+git clone https://github.com/m8r0wn/pymeta ~/Programs/pymeta
+cd ~/Programs/pymeta && python3 setup.py install
+cd
+
+git clone https://github.com/HowToFind-bot/YaSeeker ~/Programs
+sudo -H pip3 install -r ~/Programs/YaSeeker/requirements.txt
+
+git clone https://github.com/twintproject/twint.git ~/Programs/twint
+sudo -H pip3 install ~/Programs/twint
+sudo -H pip3 install -r ~/Programs/twint/requirements.txt
+
+git clone https://github.com/six2dez/reconftw ~/Programs/reconftw
+
+
+
+git clone https://github.com/HowToFind-bot/YaSeeker &&
+git clone https://github.com/CybernetiX-S3C/InfoSploit &&
+git clone https://github.com/alpkeskin/mosint &&
+git clone https://github.com/Double2Sky/GhostRecon &&
+git clone https://github.com/m3n0sd0n4ld/uDork &&
+git clone https://github.com/tdh8316/Investigo &&
+git clone https://github.com/jsvine/waybackpack &&
+git clone https://github.com/enemy-submarine/pidrila &&
+git clone https://github.com/DedSecInside/TorBot &&
+git clone https://github.com/WebBreacher/WhatsMyName &&
+git clone https://github.com/noptrix/nullscan &&
+git clone https://github.com/kpcyrd/sn0int &&
+git clone https://github.com/abhisharma404/vault &&
+git clone https://github.com/HA71/Namechk &&
+git clone https://github.com/mxrch/GHunt &&
+git clone https://github.com/AhmedConstant/lazyGrandma &&
+git clone https://github.com/0xknown/Search4 &&
+git clone https://github.com/Zarcolio/sitedorks &&
+git clone https://github.com/Ekultek/WhatBreach &&
+git clone https://github.com/chriswmorris/Metaforge &&
+git clone https://github.com/saeeddhqan/Maryam &&
+git clone https://github.com/s0md3v/Striker &&
+git clone https://github.com/gocaio/goca &&
+git clone https://github.com/bhavsec/reconspider &&
+git clone https://github.com/vysecurity/autovpn &&
+git clone https://github.com/GeneralTesler/deluxe &&
+git clone https://github.com/eschultze/URLextractor &&
+git clone https://github.com/devanshbatham/Gorecon &&
+git clone https://github.com/m1n64/MagmaOsint &&
+git clone https://github.com/Anon-Exploiter/SiteBroker &&
+git clone https://github.com/abaykan/53R3N17Y &&
+git clone https://github.com/sundowndev/PhoneInfoga &&
+git clone https://github.com/restanse/NicknameFinder &&
+git clone https://github.com/shmilylty/OneForAll &&
+git clone https://github.com/m4ll0k/Infoga &&
+git clone https://github.com/saeeddhqan/evine &&
+git clone https://github.com/noptrix/lulzbuster &&
+git clone https://github.com/Cignoraptor-ita/cignotrack &&
+git clone https://github.com/0xInfection/TIDoS-Framework &&
+git clone https://github.com/megadose/holehe &&
+git clone https://github.com/kennbroorg/iKy &&
+git clone https://github.com/sham00n/buster &&
+git clone https://github.com/jocephus/WikiLeaker &&
+git clone https://github.com/hatlord/Spiderpig &&
+git clone https://github.com/m8r0wn/nullinux &&
+git clone https://github.com/r3vn/badKarma &&
+git clone https://github.com/1N3/Goohak &&
+git clone https://github.com/twelvesec/gasmask &&
+git clone https://github.com/jcesarstef/seosint &&
+git clone https://github.com/ScriptnKitten/scriptnOsint &&
+git clone https://github.com/Shiva108/OSIRA &&
+git clone https://github.com/qeeqbox/social-analyzer &&
+git clone https://github.com/adnane-X-tebbaa/Katana &&
+git clone https://github.com/Tuhinshubhra/RED_HAWK &&
+git clone https://github.com/IvanGlinkin/Fast-Google-Dorks-Scan &&
+git clone https://github.com/SharadKumar97/OSINT-SPY &&
+git clone https://github.com/GerbenJavado/LinkFinder &&
+git clone https://github.com/eescanilla/OSINTBashKALI &&
+git clone https://github.com/radenvodka/Recsech&&
+git clone https://github.com/woj-ciech/LeakLooker-X &&
+git clone https://github.com/m8r0wn/pymeta &&
+git clone https://github.com/maurosoria/dirsearch &&
+git clone https://github.com/ThomasPWy/drwho.sh &&
+git clone https://github.com/Quantika14/email-osint-ripper 
 
 
 
