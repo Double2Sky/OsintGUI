06d4f097c553e49c6822163eb233e93ba3d8c454
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-05-24 16:34:07 +0300

    Create netshare.bat

---
+++ netshare.bat
@@ -0,0 +1,7 @@
+net share NameFolder=C:\folder /GRANT:username,CHANGE
+
+
+# NameFolder - имя папки в сети
+# C:\folder - абсолютный путь к расшариваемой папке на диске сервера
+# username - имя пользователя в сети
+# CHANGE - права, выдаваемые пользователю. принимает значения READ,CHANGE,FULL
