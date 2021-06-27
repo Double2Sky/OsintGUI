3ed1045bff9390b23b2434fb08e673d237c322df
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-06-27 21:18:00 +0300

    Create reconftw.sh

---
+++ reconftw.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter Domain."
+read domain
+mkdir /home/user/Cases/$domain-reconftw
+cd /home/user/reconftw/
+./reconftw.sh -d $domain -a -o /home/user/Cases/$domain-reconftw
+
+#install reconftw at /home/user/reconftw/
