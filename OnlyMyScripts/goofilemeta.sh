ff3721a648fd4afa554af9381aac8dbb41cd1fcb
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-08-02 15:39:39 +0300

    Update goofilemeta.sh

--- OnlyMyScripts/goofilemeta.sh
+++ OnlyMyScripts/goofilemeta.sh
@@ -4,21 +4,20 @@
 
 echo "Enter domain"
 
-read -r "$(domain)"
+read -r domain
 
-mkdir -p ~/Cases/"$(domain)"
-cd ~/Cases/"$(domain)" || exit
-
+mkdir -p ~/Cases/"$domain"
+cd ~/Cases/"$domain" || exit
 {
-    goofile -d "$(domain)" -f pdf 
-    goofile -d "$(domain)" -f doc 
-    goofile -d "$(domain)" -f docx 
-    goofile -d "$(domain)" -f txt 
-    goofile -d "$(domain)" -f xls 
-    goofile -d "$(domain)" -f xlsx 
-    goofile -d "$(domain)" -f ppt
-}   >> goofile-"$(domain)".txt 
+    goofile -d "$domain" -f pdf 
+    goofile -d "$domain" -f doc 
+    goofile -d "$domain" -f docx 
+    goofile -d "$domain" -f txt 
+    goofile -d "$domain" -f xls 
+    goofile -d "$domain" -f xlsx 
+    goofile -d "$domain" -f ppt
+}   >> goofile-"$domain".txt 
 
-wget -i goofile-"$(domain)".txt --no-check-certificate
+wget -i goofile-"$domain".txt --no-check-certificate
 
 exiftool ./* >> metadata.txt
