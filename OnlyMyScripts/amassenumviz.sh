72c3f5abe38dde0e70c6adeff2b99022b2354290
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-08-11 22:15:40 +0300

    Update amassenumviz.sh

--- OnlyMyScripts/amassenumviz.sh
+++ OnlyMyScripts/amassenumviz.sh
@@ -11,10 +11,26 @@ cd "$HOME/Cases/$domain" || exit
 amass intel -active -whois -d "$domain" | tee -a amass-"$domain".txt
 gedit amass-"$domain".txt &
 
-amass enum -passive -d "$domain"
-amass enum -active -d "$domain" 
-amass viz -d3 -o "$HOME/Cases/$domain"
+#file="$HOME/Cases/$domain/amass-$domain.txt"
 
-screen -d -m firefix amass_d3.html
+#if zenity --question --text="Would you like to open the findings in a browser?"; then
+#		while read -r links; do 
+#		    echo "Opening: $links"; screen -d -m firefox "$links"
+#		done < "$file"
+#    else
+#		echo "Done."
+#fi 
+
+amass enum -passive -d "$domain" | tee -a amass-"$domain"-sub.txt
+amass enum -active -d "$domain" | tee -a amass-"$domain"-sub.txt
+amass viz -d3 -d "$domain" -o "$HOME/Cases/$domain"
+
+screen -d -m firefix "$HOME/Cases/$domain/amass_d3.html"
+
+FILE="amass-"$domain"-sub.txt"
+
+while read -r links; do 
+	echo "Opening: $links"; screen -d -m firefox "$links"
+done < "$FILE"
 
 thunar "$HOME/Cases/$domain" >/dev/null 2>&1
