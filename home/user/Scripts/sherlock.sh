24d40d56348f2abf0382b784ba64ceec81f5c934
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-10-06 22:33:07 +0300

    Update sherlock.sh

--- home/user/Scripts/sherlock.sh
+++ home/user/Scripts/sherlock.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 url=$(zenity --entry --title "Sherlock" --text "Enter target Username" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
 if [ -n "$url" ]; then
-	sherlock "$url" --csv -o /root/Cases/"$url".csv | zenity --progress --pulsate --no-cancel --auto-close --title="Sherlock Downloader" --text="Report being saved to /root/Cases/" 2> >(grep -v 'GtkDialog' >&2)
+	cd ~/Tools/sherlock/sherlock
+	python3 sherlock.py "$url" --csv -o ~/Cases/"$url".csv | zenity --progress --pulsate --no-cancel --auto-close --title="Sherlock Downloader" --text="Report being saved to ~/Cases/" 2> >(grep -v 'GtkDialog' >&2)
 	sleep 2
-	thunar /root/Cases/ >/dev/null 2>&1
+	thunar ~/Cases/ >/dev/null 2>&1
 else
 	zenity --error --text "Missing Username, exiting"
    	exit
 fi 
+
