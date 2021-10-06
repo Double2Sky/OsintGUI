32d4fd107f701e27279a3ca73e0f2a8d3bc1cdde
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-10-06 22:34:26 +0300

    Update youtubedl.sh

--- home/user/Scripts/youtubedl.sh
+++ home/user/Scripts/youtubedl.sh
@@ -2,10 +2,11 @@
 timestamp=$(date +%Y-%m-%d:%H:%M)
 url=$(zenity --entry --title "Video Downloader" --text "Enter target URL" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
 if [ -n "$url" ]; then
-	youtube-dl "$url" -o /root/Cases/Videos/"$timestamp%(title)s.%(ext)s" -i --all-subs| zenity --progress --pulsate --no-cancel --auto-close --title="Video Downloader" --text="Video being saved to ~/Videos/" 2> >(grep -v 'GtkDialog' >&2)
+	youtube-dl "$url" -o ~/Videos/"$timestamp%(title)s.%(ext)s" -i --all-subs| zenity --progress --pulsate --no-cancel --auto-close --title="Video Downloader" --text="Video being saved to ~/Videos/" 2> >(grep -v 'GtkDialog' >&2)
 	sleep 2
-	thunar /root/Cases/Videos/ >/dev/null 2>&1
+	thunar ~/Videos/ >/dev/null 2>&1
 else
 	zenity --error --text "Missing URL, exiting"
    	exit
 fi 
+
