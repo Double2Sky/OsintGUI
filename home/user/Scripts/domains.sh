6bd122b3d6455090ced2effb3587e0a14a482164
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2021-10-06 22:30:11 +0300

    Update domains.sh

--- home/user/Scripts/domains.sh
+++ home/user/Scripts/domains.sh
@@ -6,124 +6,77 @@ opt1="Amass"
 opt2="Sublist3r"
 opt3="Photon"
 opt4="TheHarvester"
-opt5="De-Cloud"
-opt6="Get Links"
-opt7="Raccoon-Scanner"
 
 timestamp=$(date +%Y-%m-%d:%H:%M)
 fqdnregex="\b((xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b"
 
-domainmenu=$(zenity  --list  --title "Domain Tool" --text "What do you want to do?" --width=400 --height=300 --radiolist  --column "Choose" --column "Option" TRUE "$opt1" FALSE "$opt2" FALSE "$opt3" FALSE "$opt4" FALSE "$opt5" FALSE "$opt6" FALSE "$opt7" 2> >(grep -v 'GtkDialog' >&2)) 
+domainmenu=$(zenity  --list  --title "Domain Tool" --text "What do you want to do?" --width=400 --height=300 --radiolist  --column "Choose" --column "Option" TRUE "$opt1" FALSE "$opt2" FALSE "$opt3" FALSE "$opt4" 2> >(grep -v 'GtkDialog' >&2)) 
 
 case $domainmenu in
-	$opt1 ) #Amass
+	"$opt1" ) #Amass
 		domain=$(zenity --entry --title "Amass" --text "Enter target domain name" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
 		if [ ! -z "$domain" ]; then
 			#verify legit domain
 			if [[ $domain =~ $fqdnregex ]]; then
 				#Run Tool
-				mkdir /root/Cases/Amass/
-				amass intel -whois -ip -src -d $domain  -o /root/Cases/Amass/$timestamp-$domain.1.txt 
-				amass enum -src -brute -d $domain -o /root/Cases/Amass/$timestamp-$domain.2.txt -d $domain
+				mkdir /home/user/Cases/Amass/ 2>/dev/null
+				amass intel -whois -ip -src -d $domain  -o /home/user/Cases/Amass/$timestamp-$domain.1.txt 
+				amass enum -src -brute -d $domain -o /home/user/Cases/Amass/$timestamp-$domain.2.txt -d $domain
 				sleep 3
-				thunar "/root/Cases/Amass/" >/dev/null 2>&1
+				thunar "/home/user/Cases/Amass/" >/dev/null 2>&1
 				exit
 			else
 				zenity --error --text "Doesn't appear to be a legitimate domain, exiting!" 2> >(grep -v 'GtkDialog' >&2)
 				exit
  			fi
 		fi;;
-			$opt2 ) #Sublist3r
+			"$opt2" ) #Sublist3r
 		domain=$(zenity --entry --title "Sublist3r" --text "Enter target domain name" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
 		if [ ! -z "$domain" ]; then
 			#verify legit domain
 			if [[ $domain =~ $fqdnregex ]]; then
 				#Run Tool
-				mkdir /root/Cases/Sublist3r/
-				sublist3r -d $domain -o /root/Cases/Sublist3r/sublist3r_$domain.txt
+				mkdir /home/user/Cases/Sublist3r/ 2>/dev/null
+				cd /home/user/Tools/Sublist3r || exit
+				python3 sublist3r.py -d $domain -o /home/user/Cases/Sublist3r/sublist3r_$domain.txt
 			else
 				zenity --error --text "Doesn't appear to be a legitimate domain, exiting!" 2> >(grep -v 'GtkDialog' >&2)
 				exit
  			fi
 		fi;;
-			$opt3 ) #Photon
+			"$opt3" ) #Photon
 		domain=$(zenity --entry --title "Photon" --text "Enter target domain name" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
 		if [ ! -z "$domain" ]; then
 			#verify legit domain
 			if [[ $domain =~ $fqdnregex ]]; then
 				#Run Tool
-				mkdir /root/Cases/Photon/
-				photon -u $domain -l 3 -t 100 -o /root/Cases/Photon/$timestamp-$domain
+				mkdir /home/user/Cases/Photon/ 2>/dev/null
+				cd /home/user/Tools/Photon || exit
+				python3 photon.py -u $domain --wayback --clone --keys -l 10 -t 100 --dns -o /home/user/Cases/Photon/$timestamp-$domain
 				sleep 3
-				thunar "/root/Cases/Photon/$timestamp-$domain" >/dev/null 2>&1
+				thunar "/home/user/Cases/Photon/$timestamp-$domain" >/dev/null 2>&1
 				exit
 			else
 				zenity --error --text "Doesn't appear to be a legitimate domain, exiting!" 2> >(grep -v 'GtkDialog' >&2)
 				exit
  			fi
 		fi;;
-			$opt4 ) #TheHarvester
+			"$opt4" ) #TheHarvester
 		domain=$(zenity --entry --title "TheHarvester" --text "Enter target domain name" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
 		if [ ! -z "$domain" ]; then
 			#verify legit domain
 			if [[ $domain =~ $fqdnregex ]]; then
 				#Run Tool
-				mkdir /root/Cases/theHarvester/
-				theHarvester -d $domain -b all -f /root/Cases/theHarvester/$timestamp-$domain.html
-				if [ -f /root/Cases/theHarvester/$timestamp-$domain.html  ]; then
-					firefox /root/Cases/theHarvester/$timestamp-$domain.html	
+				mkdir /home/user/Cases/theHarvester/ 2>/dev/null
+				python3 /home/user/Tools/theHarvester/theHarvester.py -d $domain -b all -g --screenshot /home/user/Cases/theHarvester -n -c -f /home/user/Cases/theHarvester/$timestamp-$domain.html
+				sleep 3
+				thunar "/home/user/Cases/theHarvester"
+				if [ -f /home/user/Cases/theHarvester/$timestamp-$domain.html  ]; then
+					firefox "/home/user/Cases/theHarvester/$timestamp-$domain.html"	
 				fi	
 			else
 				zenity --error --text "Doesn't appear to be a legitimate domain, exiting!" 2> >(grep -v 'GtkDialog' >&2)
 				exit
  			fi
 		fi;;
-		
-		$opt5 ) #De-Cloud
-				domain=$(zenity --entry --width=300 --title "De-Cloud the domain and find IP" --text "Enter target domain name" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
-				if [ ! -z "$domain" ]; then
-					mkdir /root/Cases/De-Cloud
-					#Run Tool
-					cd /opt/csitools
-					./decloud $domain | tee /root/Cases/De-Cloud/decloud-$domain.txt
-					filename=/root/Cases/De-Cloud/decloud-$domain.txt
-			
-					FILE=/root/Cases/De-Cloud/tmp-$domain.txt
-					if test -f "$FILE"; then
-						cat /root/Cases/De-Cloud/decloud-$domain.txt | grep "Checking" | cut -d" " -f4 | sed 's/<BR>/\n/' | sort -u | tee -a /root/Cases/De-Cloud/tmp-$domain.txt
-						cat /root/Cases/De-Cloud/tmp-$domain.txt | sed 's/<BR>/\n/' | sort -u > /tmp/tmp.txt
-						cat /tmp/tmp.txt > /root/Cases/De-Cloud/tmp-$domain.txt 
-						rm /tmp/tmp.txt	
-					else
-						cat /root/Cases/De-Cloud/decloud-$domain.txt | grep "Checking" | cut -d" " -f4 | sed 's/<BR>/\n/' | sort -u | tee /root/Cases/De-Cloud/tmp-$domain.txt
-					fi
-					gedit /root/Cases/De-Cloud/tmp-$domain.txt &		
-			if zenity --question --text="Would you like to open the findings in a browser?"; then
-				while read links; do 
-				    echo "Opening: $links"; screen -d -m firefox $links
-				done < $FILE
-				echo "Please wait for Firefox to open..."
-			else
-				echo "Done."
-			fi 
-		fi	
-		;;
-			$opt6 ) #Get Links
-				domain=$(zenity --entry --width=300 --title "Get Links from a webpage." --text "Enter target domain name" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
-				if [ ! -z "$domain" ]; then
-					mkdir /root/Cases/Get-Links
-					cd /opt/csitools
-					./getlinks $domain | tee /root/Cases/Get-Links/site-links-$domain.txt && gedit /root/Cases/Get-Links/site-links-$domain.txt &
-				fi
-				exit
-			;;
-			$opt7 ) #Raccoon-Scanner
-				domain=$(zenity --entry --width=300 --title "Running Raccoon-Scanner" --text "Enter target domain name" --entry-text "" 2> >(grep -v 'GtkDialog' >&2))
-				if [ ! -z "$domain" ]; then
-					mkdir /root/Cases/Raccoon
-					raccoon $domain -o /root/Cases/Raccoon
-					thunar /root/Cases/Raccoon
-				fi
-			exit
-		;;
 esac
