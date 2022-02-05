c9caf4163601ae9888f3c405d06f7829ce89ee96
Double2Sky <55303365+Double2Sky@users.noreply.github.com>
2022-02-05 17:42:52 +0300

    Update toutatis.sh

--- OnlyMyScripts/toutatis.sh
+++ OnlyMyScripts/toutatis.sh
@@ -2,7 +2,7 @@
 
 acname=$(zenity --entry --text="Enter Account Name (ex: navalny)")
 mkdir -p "/home/user/Cases/Toutatis/"
-    toutatis -s 50274382114%3AM6Uv7n6V9TbPfF%3A5 -u "$acname" | tee -a "/home/user/Cases/Toutatis/$acname.txt"
+    toutatis -s $(session ID) -u "$acname" | tee -a "/home/user/Cases/Toutatis/$acname.txt"
         gedit "/home/user/Cases/Toutatis/$acname.txt"
         thunar "/home/user/Cases/Toutatis" >/dev/null 2>&1
         read
